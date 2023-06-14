d51d34c71fac02958303fb35bb76f87d98c80d03
stratebi <german.berna@stratebi.com>
2023-06-14 19:56:24 +0200

    Registro de logs

--- app.js
+++ app.js
@@ -365,7 +365,11 @@ app.post('/archive', function(req, res)
     } 
 })
       
-
+// Redirigir la salida de la consola a un archivo
+const consoleOutputFile = fs.createWriteStream('./console_output.txt');
+console.log = function(message) {
+  consoleOutputFile.write(message + '\n');
+};
 
 
 app.listen(port, function(){
